d27d3ce68b78341fc5f65e5b51dca56d47ff5e50
Circlebit <paul.seibel@gmail.com>
2018-03-21 21:41:41 +0100

    sketching up some unit classes

---
+++ Bonebreaker/Unit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonebreaker
+{
+    class Unit
+    {
+        int X { get; set; }
+        int Y { get; set; }
+
+        string Name { get; set; }
+
+        public Unit(int x, int y, string name)
+        {
+            X = x;
+            Y = y;
+
+            Name = name;
+        }
+    }
+
+    class Player : Unit
+    {
+        
+    }
+}
