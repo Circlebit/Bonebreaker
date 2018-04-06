feac313c83a0f4170f6b0b749e0c305a9d84bd89
Paul Seibel <paul.seibel@mailbox.org>
2018-04-06 11:57:27 +0200

    changed project name and namespace from Bonebreaker2 to Bonebreaker

--- Bonebreaker2/Map.cs
+++ Bonebreaker2/Map.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bonebreaker2
+namespace Bonebreaker
 {
     class Map
     {
--- Bonebreaker2/Properties/AssemblyInfo.cs
+++ Bonebreaker2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bonebreaker2")]
+[assembly: AssemblyTitle("Bonebreaker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bonebreaker2")]
+[assembly: AssemblyProduct("Bonebreaker")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
